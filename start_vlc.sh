1f6e66dda13fb0f7642df92525c9ff0b302244e9
IntDev <git@internet.dev>
2025-08-15 18:55:51 -0700

    chore: remove old version of script

--- start_vlc.sh
+++ start_vlc.sh
@@ -1,9 +1,14 @@
-#!/bin/bash
+!/bin/bash
 
+LOG_FILE="/home/intdev/vlc.log"
+PID_FILE="/home/intdev/vlc.pid"
 MEDIA_DIR="/home/intdev/media"
 
-# Set up environment for Wayland
 export DISPLAY=:0
-export WAYLAND_DISPLAY=wayland-0
 
-cvlc --fullscreen --loop --no-osd --no-audio --intf dummy "$MEDIA_DIR"
+# Create/update the m3u playlist file with all video files in the directory
+find "$MEDIA_DIR" -type f -iregex '.*\.\(m4v\|mov\|mp4\|avi\|mkv\|wmv\|flv\)$' > "$PLAYLIST_FILE"
+
+nohup env DISPLAY=:0 cvlc --fullscreen --loop --no-osd --no-audio "$MEDIA_DIR/playlist.m3u"  > "$LOG_FILE" 2>&1 &
+echo $! > "$PID_FILE"
+echo "VLC started with PID: $!"
