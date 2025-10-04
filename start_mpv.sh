d130cd493e442830148316bcaa8337d8f1e87532
Jack Allard <jackallard17@gmail.com>
2025-10-03 18:24:31 -0700

    fix: handl existing IPC socket

--- start_mpv.sh
+++ start_mpv.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 MEDIA_DIR="/home/intdev/media"
 PLAYLIST_FILE="/home/intdev/media/playlist.m3u"
+SOCKET_FILE="/tmp/mpvsocket"
 
-# find "$MEDIA_DIR" -type f -iregex '.*\.\(m4v\|mov\|mp4\|avi\|mkv\|wmv\|flv\)$' > "$PLAYLIST_FILE"
+# Clean up existing socket if present
+if [ -S "$SOCKET_FILE" ]; then
+    rm -f "$SOCKET_FILE"
+fi
 
+# Generate playlist from media files
+find "$MEDIA_DIR" -type f -iregex '.*\.\(m4v\|mov\|mp4\|avi\|mkv\|wmv\|flv\)$' > "$PLAYLIST_FILE"
+
+# Start mpv with the playlist
 mpv \
     --fullscreen \
     --shuffle \
     --loop-playlist \
-    --input-ipc-server=/tmp/mpvsocket # allows us to query currently playing file with: "echo '{ "command": ["get_property", "filename"] }' | socat - /tmp/mpvsocket"
-    "$PLAYLIST_FILE" > 2>&1 &
+    --input-ipc-server="$SOCKET_FILE" \
+    "$PLAYLIST_FILE" > /dev/null 2>&1 &
 
 echo "mpv started"
